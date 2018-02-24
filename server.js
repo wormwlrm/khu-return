05f584799e8c530b1c6ff2eedd227cee7e7ac7e7
minhye-lee <31986076+minhye-lee@users.noreply.github.com>
2018-02-24 12:21:32 +0900

    Update port
    3000포트로 실행 안돼서
    3001로 옮김

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+﻿var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
 
@@ -7,6 +7,6 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public')); // 정적인 파일 서비스
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
+var server = app.listen(3001, function(){
+    console.log("Express server has started on port 3001")
 });
